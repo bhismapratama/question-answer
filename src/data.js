b7f025059805a6dc91600598eca66e73c5f79d99
bhismapratama <bhismaelki@gmail.com>
2023-06-22 20:22:40 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,45 @@
+const questions = [
+  {
+    id: 1,
+    tanya: 'Apa ibu Kota Australia?',
+    info:
+      '( Jawaban kamu )',
+  },
+  {
+    id: 2,
+    tanya: 'Siapa penulis novel “The Catcher in the Rye”?',
+    info:
+      '( Jawaban kamu )',
+  },
+  {
+    id: 3,
+    tanya: 'Planet terkecil dalam tata surya?',
+    info:
+      '( Jawaban kamu )',
+  },
+  {
+    id: 4,
+    tanya: 'Binatang mamalia terbesar di dunia?',
+    info:
+      '( Jawaban kamu )',
+  },
+  {
+    id: 5,
+    tanya: 'Apa nama sungai terpanjang di dunia?',
+    info:
+      '( Jawaban kamu )',
+  },
+  {
+    id: 6,
+    tanya: 'Apa nama gunung tertinggi di dunia?',
+    info:
+      '( Jawaban kamu )',
+  },
+  {
+    id: 7,
+    tanya: 'Apa nama gurun pasir terbesar di dunia?',
+    info:
+      '( Jawaban kamu )',
+  },
+]
+export default questions
